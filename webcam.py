e3d2edef79a8f84f949a9e9adb741361638e3556
AmirHossein Teymoori <teymoori.net@gmail.com>
2023-11-12 22:34:45 +0100

    codes updated

--- webcam.py
+++ webcam.py
@@ -1,37 +1,49 @@
 import cv2
+import numpy as np
+import tensorflow as tf
 
-class WebcamVideoStream:
-    def __init__(self, src=0):
-        # Initialize the video camera stream and read the first frame
-        self.stream = cv2.VideoCapture(src)
-        if not self.stream.isOpened():
-            raise IOError("Cannot open webcam")
-        self.ret, self.frame = self.stream.read()
+# Load the saved model
+model = tf.saved_model.load('ssd_mobilenet_v2_320x320_coco17_tpu-8/saved_model')
 
-    def start(self):
-        # Start the video stream
-        while True:
-            self.ret, self.frame = self.stream.read()
-            if not self.ret:
-                break
-            cv2.imshow('Webcam', self.frame)
+# Get the function for performing inference
+infer = model.signatures['serving_default']
 
-            # 'q' key has been pressed, exit the loop
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+# Start video capture
+cap = cv2.VideoCapture(0)
 
-    def release(self):
-        # Release the camera and close all OpenCV windows
-        self.stream.release()
-        cv2.destroyAllWindows()
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # Convert the BGR image to RGB before processing.
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # Preprocess the frame for the model
+    input_tensor = tf.convert_to_tensor([rgb_frame], dtype=tf.uint8)
+    
+    # Perform the detection
+    detections = infer(input_tensor)
+    
+    # Draw detections on the frame
+    detection_boxes = detections['detection_boxes'].numpy()[0]
+    detection_scores = detections['detection_scores'].numpy()[0]
+    detection_classes = detections['detection_classes'].numpy()[0]
+    height, width, _ = frame.shape
 
-# Use the webcam video stream class
-if __name__ == '__main__':
-    # Initialize webcam video stream object for USB webcam
-    webcam_stream = WebcamVideoStream(src=0)  # Assuming '1' is the USB webcam index
+    for i in range(len(detection_scores)):
+        if detection_scores[i] > 0.5:  # Only consider detections with a score above 0.5
+            ymin, xmin, ymax, xmax = detection_boxes[i]
+            (left, right, top, bottom) = (xmin * width, xmax * width,
+                                          ymin * height, ymax * height)
+            # Draw rectangle to frame
+            cv2.rectangle(frame, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 2)
 
-    # Start capturing video
-    webcam_stream.start()
+    # Display the frame
+    cv2.imshow('Object Detection', frame)
 
-    # Release resources
-    webcam_stream.release()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
